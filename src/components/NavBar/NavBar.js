2625fc1d6a47fdbbff7b6620dc66d6422c7d9624
Piett-rm <rmpetit24@gmail.com>
2024-10-16 16:04:06 +0200

    fix(navBar): fix de la sensibilisation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,32 +6,10 @@ function NavBar() {
     return (
         <div className="NavBar">
             <ChakraLink as={ReactRouterLink} to='/'>Accueil</ChakraLink>
-            <ChakraLink as={ReactRouterLink} to='/sensibilisation'>Sensibilisation</ChakraLink>
+            <ChakraLink as={ReactRouterLink} to='/sensibilisation/racisme'>Sensibilisation</ChakraLink>
             <ChakraLink as={ReactRouterLink} to='/fake-message'>Fake Message</ChakraLink>
             <ChakraLink as={ReactRouterLink} to='/contact'>Nous contacter</ChakraLink>
         </div>
-        // <Menu>
-        //     <MenuButton
-        //         as={IconButton}
-        //         aria-label='Options'
-        //         icon={<HamburgerIcon />}
-        //         variant='outline'
-        //     />
-        //     <MenuList>
-        //         <MenuItem icon={<AddIcon />} command='⌘T'>
-        //             New Tab
-        //         </MenuItem>
-        //         <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
-        //             New Window
-        //         </MenuItem>
-        //         <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
-        //             Open Closed Tab
-        //         </MenuItem>
-        //         <MenuItem icon={<EditIcon />} command='⌘O'>
-        //             Open File...
-        //         </MenuItem>
-        //     </MenuList>
-        // </Menu>
     )
 }
 
